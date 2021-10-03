08dbd6fa170f13103cea6b09d41da57b48e351cf
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-10-03 09:24:39 +0700

    ++Come round, Come round. All the finest method for the most vicious librarian in need

---
+++ Second_Question(Library)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Second_Question_Library_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
